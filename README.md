16571de1ce83408dee9887dac741553cb16423e0
Jeo Araujo <jeostonjunior@gmail.com>
2026-03-25 22:37:54 -0300

    feat: infraestrutura inicial do PostgreSQL com Docker e schema das tabelas

---
+++ README.md
@@ -0,0 +1,2 @@
+# Pipeline de Dados com Apache Hop e PostgreSQL
+Repositório do case prático de extração do Currículo Lattes.
---
+++ init_db.sh
@@ -0,0 +1,7 @@
+psql -U postgres -d postgres -c "CREATE DATABASE BD_PESQUISADOR
+WITH
+OWNER = postgres
+ENCODING = 'UTF8'
+TABLESPACE = pg_default
+CONNECTION LIMIT = -1
+IS_TEMPLATE = False;"
